9876dde67a069a769859b05a55e9dc17faafc3fc
nominik-spb <61251151+nominik-spb@users.noreply.github.com>
2025-05-14 15:26:56 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        List<String> firstClients = List.of("Anya", "Sveta", "Olya", "Alexandra", "Ruslana", "Olesya", "Vika");
+
+        Queue<String> queue = new LinkedList<>();
+
+        queue.addAll(firstClients);
+
+        while (!queue.isEmpty()) {
+
+            if (Math.random() < 0.5) { // проверка условия, которое срабатывает с 50% вероятностью
+                System.out.println(queue.peek() + " сделала маникюр");
+                queue.add("frends of " + queue.peek());
+                System.out.println(queue.poll() + " порекомендовала друга");
+            } else {
+                System.out.println(queue.poll() + " сделала маникюр");
+            }
+
+        }
     }
 }
